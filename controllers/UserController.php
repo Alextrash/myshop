dabeb4d8e007102ad4fd6ae71d01eba5f05d3555
alex <alex@example.com>
2016-07-18 23:17:55 +0300

    home update

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,4 +1,23 @@
 <?php
 /**
- *
+ * Контроллер функций пользователя
  */
+
+include_once '../models/CategoriesModel.php';
+include_once '../models/UsersModel.php';
+
+function registerAction($mysqli){
+    $email = isset($_REQUEST['email']) ? $_REQUEST['email'] : null;
+    $email = trim($email);
+
+    $pwd1 = isset($_REQUEST['pwd1']) ? $_REQUEST['pwd1'] : null;
+    $pwd2 = isset($_REQUEST['pwd2']) ? $_REQUEST['pwd2'] : null;
+
+    $phone = isset($_REQUEST['phone']) ? $_REQUEST['phone'] : null;
+    $address = isset($_REQUEST['address']) ? $_REQUEST['address'] : null;
+    $name = isset($_REQUEST['name']) ? $_REQUEST['name'] : null;
+    $name = trim($name);
+
+    $resData = null;
+    $resData = checkRegisterParams($email, $pwd1, $pwd2);
+}
--- models/UsersModel.php
+++ models/UsersModel.php
@@ -1,7 +1,75 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Core
- * Date: 18.07.2016
- * Time: 15:40
+ * Модель для таблицв пользователей (users)
  */
+/**
+ * @param $email
+ * @param $pwdMD5
+ * @param $name
+ * @param $phone
+ * @param $address
+ * @param $mysqli
+ * return array   данные пользователя
+ */
+function registerNewUser($email, $pwdMD5, $name, $phone, $address, $mysqli){
+    $email = htmlspecialchars($mysqli->escape_string($email));
+    $pwdMD5 = htmlspecialchars($mysqli->escape_string($pwdMD5));
+    $name = htmlspecialchars($mysqli->escape_string($name));
+    $phone = htmlspecialchars($mysqli->escape_string($phone));
+    $address = htmlspecialchars($mysqli->escape_string($address));
+
+    $sql = "INSERT INTO
+            `users` (`email`, `pwd`, `name`, `phone`, `address`)
+            VALUES (`$email`, `$pwdMD5`, `$name`, `$phone`, `$address`)";
+
+    $rs = $mysqli->query($sql);
+
+    if($rs){
+        $sql = "SELECT *
+                FROM `users`
+                WHERE (`email` = '{$email}' and `pwd` = '{$pwdMD5}')
+                LIMIT 1";
+
+        $rs = $mysqli->query($sql);
+        $rs = createSmartyRsArray($rs, $mysqli);
+
+        if(isset($rs[0])){
+            $rs['success'] = 1;
+        } else $rs['success'] = 0;
+
+    }else $rs['success'] = 0;
+
+    return $rs;
+}
+
+
+/**
+ * Проверяем переменные
+ * @param $email
+ * @param $pwd1
+ * @param $pwd2
+ * @return array Описание ошибки
+ */
+function сheckRegisterParams($email, $pwd1, $pwd2){
+
+    $res = array();
+
+    if( ! $email) {
+        $res['success'] = null;
+        $res['message'] = 'Введите e-mail';
+    }
+    if( ! $pwd1){
+        $res['success'] = null;
+        $res['message'] = 'Введите пароль';
+    }
+    if( ! $pwd2){
+        $res['success'] = null;
+        $res['message'] = 'Введите повтор пароля';
+    }
+    if($pwd1 != $pwd2){
+        $res['success'] = null;
+        $res['message'] = 'Ошибка! Пароли не совпадают';
+    }
+
+    return $res;
+}
